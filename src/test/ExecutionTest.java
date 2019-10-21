8eb1813843bac51b7e80e70eb181ab345e7f8a94
Pablo Aliaga <pablo.aliaga@kapsch.net>
2019-10-21 00:01:40 -0300

    Project created

---
+++ src/test/ExecutionTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ExecutionTest {
+
+	private WebDriver driver;
+	private String url = "http://www.aliexpress.com";
+
+	private String getWebDriverPath() {
+		return System.getProperty("user.dir").concat(File.separator).concat("webDriver").concat(File.separator).concat("chromedriver.exe");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver", getWebDriverPath());
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+	}
+
+	@Test
+	public void smokeTest() {
+		System.out.println(">>> The test has started.");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.close();
+		driver.quit();
+	}
+}
